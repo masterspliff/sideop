c8c9be3f10d6e3c63741a46a680011ec0e15570f
Masterspliff <obnorup@gmail.com>
2024-02-18 17:16:01 +0100

    hej

--- src/components/HomeModule.vue
+++ src/components/HomeModule.vue
@@ -4,7 +4,7 @@
       <div v-for="item in items" :key="item.title" class="section flex-1 bg-black text-white flex flex-col justify-center items-center group">
         <img :src="item.image" :alt="item.alt" class="mb-4">
         <h2 class="text-2xl font-bold mb-2">{{ item.title }}</h2>
-        <a href="#" class="text-gray-300">Placeholder Link</a>
+        <a href="#" class="text-gray-300"></a>
       </div>
     </div>
   </div>
@@ -32,18 +32,55 @@ export default {
 </script>
 
 <style scoped>
+.section {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  transition: flex-grow 0.3s ease;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+
 img {
   object-fit: cover;
   width: 100%;
-  height: auto;
+  height: 100%;
+  filter: grayscale(100%);
+  transition: filter 0.3s ease;
 }
 
-.section {
-  transition: flex-grow 0.3s ease;
-  cursor: pointer;
+.section:hover img {
+  filter: grayscale(0);
+}
+
+/* Style text for outline effect */
+.section h2, .section a {
+  position: absolute;
+  -webkit-text-fill-color: transparent; /* Use for Chrome and Safari */
+  text-fill-color: transparent; /* Standard syntax; currently only works in Firefox */
+  -webkit-text-stroke-width: 5px; /* Define the width of the stroke */
+  -webkit-text-stroke-color: black; /* Define the color of the stroke */
+  text-stroke-width: 1px; /* Standard syntax; currently only works in Firefox */
+  text-stroke-color: black; /* Standard syntax; currently only works in Firefox */
+  font-size: 5em; /* Larger text size by default */
+  transition: all 0.3s ease; /* Transition for text styling changes */
+  white-space: nowrap; /* Prevent text from wrapping */
+}
+
+/* Change text style on hover */
+.section:hover h2, .section:hover a {
+  -webkit-text-fill-color: white; /* Fill text color on hover for Chrome and Safari */
+  text-fill-color: white; /* Standard syntax; currently only works in Firefox */
+  -webkit-text-stroke-width: 0px; /* Remove stroke on hover */
+  text-stroke-width: 0px; /* Standard syntax; currently only works in Firefox */
 }
 
 .section:hover {
   flex-grow: 5;
 }
 </style>
+
+
