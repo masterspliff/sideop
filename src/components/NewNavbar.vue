0489dd057bff6fd164e352d59c21a7dc0625560c
Masterspliff <obnorup@gmail.com>
2024-02-18 19:11:49 +0100

    router link

--- src/components/NewNavbar.vue
+++ src/components/NewNavbar.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="fixed top-0 z-50 w-full">
-      <nav class=" w-full items-center justify-between p-4 lg:p-6 bg-gradient-to-t from-transparent to-black" aria-label="Global">
+      <nav class="w-full items-center justify-between p-4 lg:p-6 bg-gradient-to-t from-transparent to-black" aria-label="Global">
         <Disclosure as="nav" v-slot="{ open }">
           <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div class="relative flex h-16 items-center justify-between">
@@ -21,7 +21,7 @@
                 <!-- Modified container to align links to the right -->
                 <div class="hidden sm:ml-6 sm:block sm:flex-1 sm:flex sm:items-center sm:justify-end">
                   <div class="flex space-x-4">
-                    <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 nav-link', 'rounded-md px-3 py-2 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+                    <router-link v-for="item in navigation" :key="item.name" :to="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 nav-link', 'rounded-md px-3 py-2 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</router-link>
                   </div>
                 </div>
               </div>
@@ -30,13 +30,14 @@
   
           <DisclosurePanel class="sm:hidden">
             <div class="space-y-1 px-2 pb-3 pt-2">
-              <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 nav-link', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+              <router-link v-for="item in navigation" :key="item.name" :to="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 nav-link', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</router-link>
             </div>
           </DisclosurePanel>
         </Disclosure>
       </nav>
     </header>
   </template>
+  
   
 
   
